909b87340eda4e73546d24e8d9ad7bb831c89392
Dadaaard <ramedward322@gmail.com>
2025-02-05 04:41:20 +0800

    Fetched available gym class

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Http\Request;
+use App\Models\ScheduledClass;
+use Illuminate\Support\Facades\Auth;
 
 class BookingController extends Controller
 {
-    //
+    
+    
+    public function index()
+    {
+        return view('member.bookings');
+    }
+
+    public function create()
+    {
+        $scheduledClasses = ScheduledClass::where('date_time', '>', now())->oldest()->get();
+       
+
+        return view('member.book', compact('scheduledClasses'));
+    }
+
+    public function store(Request $request)
+    {
+
+        dd(Auth::check());
+        // $validated = $request->validate([
+        //     'scheduled_class_id' => 'required',
+        // ]);
+
+        // $scheduledClass = ScheduledClass::find($validated['scheduled_class_id']);
+        // $scheduledClass->members()->attach(Auth::user()->id);
+
+        // return redirect()->back()->with('success', 'Class scheduled successfully');
+    }
+
+    public function destroy()
+    {
+        //
+    }
 }
